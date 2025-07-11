7671af44b224ef4491d14cf66711810cafb2def2
DDUPA <SnakePyQC@126.com>
2025-07-11 16:16:35 +0800

    add

--- src/pages/login/components/login-form.tsx
+++ src/pages/login/components/login-form.tsx
@@ -73,7 +73,7 @@ export default function LoginForm() {
 
   return (
     <Form
-      initialValues={{ username: "admin", password: "12345678" }}
+      initialValues={{ username: "", password: "" }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
