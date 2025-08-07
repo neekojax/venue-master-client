6ea5748394c34112b00c4f69cc73101e428b77cf
cherry <1379542956@qq.com>
2025-08-07 17:02:17 +0800

    save change

---
+++ dist/js/index-DA1Q29z9.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-JrZjfwQc.js";import{O as o}from"./react-BIszHAjf.js";import"./antd-B78Ig_h7.js";function a(){return t.jsx("div",{className:"text-indigo-700",children:t.jsx(o,{})})}export{a as default};
