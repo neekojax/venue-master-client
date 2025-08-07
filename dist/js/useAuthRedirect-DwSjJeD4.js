373938ac76c0dd2bd7d35f93d24fc9245000ab1d
cherry <1379542956@qq.com>
2025-08-07 16:56:33 +0800

    save change

--- dist/js/useAuthRedirect-DwSjJeD4.js
+++
@@ -1,1 +0,0 @@
-import{u as o,r as a}from"./react-BIszHAjf.js";import{e as r}from"./fetchHelper-B3LAxhEM.js";const n=()=>{const e=o();a.useEffect(()=>{const t=s=>{e(s)};return r.on("redirect",t),()=>{r.off("redirect",t)}},[e])};export{n as u};
