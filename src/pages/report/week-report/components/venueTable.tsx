14073e3366d6208593a603a292ccded8a3f3b168
cherry <1379542956@qq.com>
2025-09-15 00:59:04 +0800

    save

--- src/pages/report/week-report/components/venueTable.tsx
+++ src/pages/report/week-report/components/venueTable.tsx
@@ -42,6 +42,7 @@ const VenuePage: React.FC<VenueTableProps> = ({ data }) => {
       key: "venue_name",
       align: "left",
       sorter: (a: DataItem, b: DataItem) => a.venue_name.localeCompare(b.venue_name),
+      defaultSortOrder: "ascend", // ✅ 默认升序
       render: (text: string, record: { venue_name?: any }) => {
         const venue_id = record?.venue_name.split("-")[0] || 0;
         const isSpecialVenue = text === "Arct-HF01-J XP-AR-US" || text === "ARCT Technologies-HF02-AR-US";
