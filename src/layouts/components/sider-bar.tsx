384e672ec300a12457b6ed0c89501f4164e92c6c
cherry <1379542956@qq.com>
2025-11-19 21:58:08 +0800

    feat: save change

--- src/layouts/components/sider-bar.tsx
+++ src/layouts/components/sider-bar.tsx
@@ -74,10 +74,14 @@ const SiderItems = () => {
         //   key: ROUTE_PATHS.venueWeather,
         //   label: <Link to={ROUTE_PATHS.venueWeather}>场地天气</Link>,
         // },
-        {
-          key: ROUTE_PATHS.venueBill,
-          label: <Link to={ROUTE_PATHS.venueBill}>场地账单</Link>,
-        },
+        ...(showNDPoolType == "CANG"
+          ? [
+              {
+                key: ROUTE_PATHS.venueBill,
+                label: <Link to={ROUTE_PATHS.venueBill}>场地账单</Link>,
+              },
+            ]
+          : []),
         {
           key: ROUTE_PATHS.eventLog,
           label: <Link to={ROUTE_PATHS.eventLog}>事件日志</Link>,
