236e9c601603f1c9cf9e96dc468b071c84ab4892
cherry <1379542956@qq.com>
2026-03-17 18:30:18 +0800

    feat: save chane

--- src/pages/venue/venue-setting/index.tsx
+++ src/pages/venue/venue-setting/index.tsx
@@ -232,7 +232,7 @@ const VenueManagement: React.FC = () => {
       sorter: (a: Venue, b: Venue) => a.venue_name.localeCompare(b.venue_name),
     },
     {
-      title: "矿池",
+      title: "子账户",
       dataIndex: "pools",
       width: 200,
       render: (pools: Pool[]) => {
@@ -244,7 +244,7 @@ const VenueManagement: React.FC = () => {
                 const color = s === 1 ? "green" : s === 0 ? "red" : s === 2 ? "orange" : "#666";
                 const text = s === 1 ? "活跃" : s === 0 ? "关机" : s === 2 ? "已撤场" : "未知";
                 return (
-                  <span key={`${pool.pool_id}-${idx}`} style={{ whiteSpace: "nowrap" }}>
+                  <span key={`${pool.pool_id}-${idx}`} style={{ whiteSpace: "nowrap", display: "block" }}>
                     <span style={{ color: "#333" }}>{pool.pool_name}</span>
                     <span style={{ color, marginLeft: 4 }}>（{text}）</span>
                   </span>
