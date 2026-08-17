b18668057c1724b1ab9ac37e1c5f60d90f464b76
cherry <1379542956@qq.com>
2026-08-17 16:40:16 +0800

    feat: save change

--- dist/js/index-B0KRyIer.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-BcHsEqCq.js";import{O as o}from"./react-BIszHAjf.js";import"./antd-ByUqjMiu.js";function a(){return t.jsx("div",{className:"text-indigo-700",children:t.jsx(o,{})})}export{a as default};
